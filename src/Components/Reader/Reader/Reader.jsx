200c630b7553154c1bfcace02a757fe82146be9b
Sergei Mikhailenko <sersus1991@gmail.com>
2023-02-01 13:51:46 +0200

    reader component with hooks

--- src/Components/Reader/Reader/Reader.jsx
+++ src/Components/Reader/Reader/Reader.jsx
@@ -1,55 +1,47 @@
-import { PureComponent } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Box } from 'Components/Common';
 import { Controls } from 'Components/Reader/Controls';
 import { Progress } from 'Components/Reader/Progress';
 import { Publication } from 'Components/Reader/Publication';
 
-export class Reader extends PureComponent {
-  state = {
-    index: 0,
-    swiperSide: 0,
-  };
+export function Reader({ items }) {
+  const [index, setIndex] = useState(0);
+  const [swiperSide, setSwiperSide] = useState(0);
+  const fistRender = useRef(true);
 
-  componentDidMount() {
-    // console.log('mount');
+  useEffect(() => {
     const savedIndex = Number(localStorage.getItem('currentPublication'));
-    if (savedIndex) {
-      this.setState({ index: savedIndex });
+    if (fistRender.current && savedIndex) {
+      console.log('fist render');
+      fistRender.current = false;
+      setIndex(savedIndex);
     }
-  }
 
-  componentDidUpdate(_, pS) {
-    // console.log('update');
-    if (this.state.index !== pS.index) {
-      localStorage.setItem('currentPublication', this.state.index);
-    }
-  }
+    console.log('render');
+    localStorage.setItem('currentPublication', index);
+  }, [index]);
 
-  handleBtnClick = value => {
-    this.setState(pS => ({ index: pS.index + value }));
-    this.setState({ swiperSide: value });
+  const handleBtnClick = value => {
+    setIndex(pI => pI + value);
+    setSwiperSide(value);
   };
 
-  render() {
-    const { index, swiperSide } = this.state;
-    const { items } = this.props;
-    const currentItem = items[index];
-    const { title, text } = currentItem;
-    const currentIndex = index + 1;
-    const itemsLength = items.length;
-    const isFist = index === 0;
-    const isLast = index === itemsLength - 1;
+  const currentItem = items[index];
+  const { title, text } = currentItem;
+  const currentIndex = index + 1;
+  const itemsLength = items.length;
+  const isFist = index === 0;
+  const isLast = index === itemsLength - 1;
 
-    return (
-      <Box width="card" mx="auto" p={4}>
-        <Controls
-          onClick={this.handleBtnClick}
-          prevBtnDisabled={isFist}
-          nextBtnDisabled={isLast}
-        />
-        <Publication title={title} text={text} isRigthSwipe={swiperSide} />
-        <Progress currentPage={currentIndex} totalPages={itemsLength} />
-      </Box>
-    );
-  }
+  return (
+    <Box width="card" mx="auto" p={4}>
+      <Controls
+        onClick={handleBtnClick}
+        prevBtnDisabled={isFist}
+        nextBtnDisabled={isLast}
+      />
+      <Publication title={title} text={text} isRigthSwipe={swiperSide} />
+      <Progress currentPage={currentIndex} totalPages={itemsLength} />
+    </Box>
+  );
 }
