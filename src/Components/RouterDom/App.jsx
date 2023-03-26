3df9d0ececacbee406fd5ad0e391858d9f2c7a55
Sergei Mikhailenko <sersus1991@gmail.com>
2023-03-26 21:58:22 +0300

    add browserRouter template

---
+++ src/Components/RouterDom/App.jsx
@@ -0,0 +1,9 @@
+import { Box } from 'Components/Common';
+
+export const App = () => {
+  return (
+    <Box>
+      <Box></Box>
+    </Box>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 // import { App } from './Components/App';
-import { App } from './Components/State/App';
+// import { App } from './Components/State/App';
 // import { App } from './Components/Todos/App/App';
 // import { App } from './Components/Reader/App';
 // import { App } from './Components/Pokemon/App';
 // import { App } from './Components/Materials/App';
+import { App } from 'Components/RouterDom/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './constants/thema';
 
@@ -17,7 +19,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
