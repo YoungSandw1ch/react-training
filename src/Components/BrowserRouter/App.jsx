4b3a1060e3ba974411aa10bf075c2d155b4efac9
Sergei Mikhailenko <sersus1991@gmail.com>
2023-03-26 23:01:00 +0300

    add layout

--- src/Components/BrowserRouter/App.jsx
+++ src/Components/BrowserRouter/App.jsx
@@ -1,31 +1,24 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
-import { Box } from 'Components/Common';
+import { Route, Routes } from 'react-router-dom';
 import Homepage from 'Project/BrowserRouter/Pages/Home';
 import Contacts from 'Project/BrowserRouter/Pages/Contacts';
 import Dogs from 'Project/BrowserRouter/Pages/Dogs';
 import DogsDetails from 'Project/BrowserRouter/Pages/DogDetails';
 import { Subbreads } from './Subbreads/Subbreads';
 import { Gallery } from './Gallery/Gallery';
+import { Layout } from './Layout/Layout';
 
 export const App = () => {
   return (
-    <Box>
-      <header>
-        <nav>
-          <NavLink to="/">Homepage</NavLink>
-          <NavLink to="/contacts">Contacts</NavLink>
-          <NavLink to="/dogs">Dogs</NavLink>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/dogs" element={<Dogs />} />
-        <Route path="/dogs/:dogId" element={<DogsDetails />}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path="contacts" element={<Contacts />} />
+        <Route path="dogs" element={<Dogs />} />
+        <Route path="dogs/:dogId" element={<DogsDetails />}>
           <Route path="subbreads" element={<Subbreads />} />
           <Route path="gallery" element={<Gallery />} />
         </Route>
-      </Routes>
-    </Box>
+      </Route>
+    </Routes>
   );
 };
--- src/Components/BrowserRouter/Layout/Layout.jsx
+++ src/Components/BrowserRouter/Layout/Layout.jsx
@@ -1,1 +1,18 @@
-export const Layout = () => {};
+import { NavLink, Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <NavLink to="/">Homepage</NavLink>
+          <NavLink to="/contacts">Contacts</NavLink>
+          <NavLink to="/dogs">Dogs</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
